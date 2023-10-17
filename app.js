847db8eb3b31c0089ff4d13879e1001406c8ea3c
JuanRCoder <guillecarck@gmail.com>
2023-10-17 14:51:20 -0500

    update javascript

--- app.js
+++ app.js
@@ -1,19 +1,31 @@
 const btn = document.querySelector('.button');
 const entrada = document.querySelector('.filter');
-const caja = document.querySelector('.grafic');
+let caja = document.querySelector('.grafic');
 
-function MostrarData(data,departament) {
-    // let departaments = data.filter(dep => dep.NOMBDEP === departament);
-    // caja.innerHTML = "";
-    console.log(data, departament);  
-}
+function MostrarData(data, input) {
+    let distritos = data.filter(obj => obj && obj.NOMBDEP == input);
+    let count = distritos.length;
+    console.log(`Departamento: ${input}, cantidad de Distritos: ${count}`);
+};
+
+
 
 btn.addEventListener('click', () => {
-    const val = entrada.value;
+    const input = entrada.value.toUpperCase();
     
     // Realizar la solicitud usando fetch
     fetch("https://raw.githubusercontent.com/0skarmp/FirstProjectByGroup/main/UBIGEO.json")
     .then(res => res.json())
-    .then(data => MostrarData(data, val))
+    .then(data => MostrarData(data, input))
     .catch(error => console.log(error));
 });
+
+// {
+//     "IDDIST": "010101",
+//     "NOMBDEP": "AMAZONAS",
+//     "NOMBPROV": "CHACHAPOYAS",
+//     "NOMBDIST": "CHACHAPOYAS",
+//     "NOM_CAPITAL (LEGAL)": "CHACHAPOYAS",
+//     "COD_ REG_NAT": 2,
+//     "REGION NATURAL": "SIERRA"
+//    },

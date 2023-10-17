092a8cdfbb8d8411c97e36def32eeff24b754a2f
JuanRCoder <guillecarck@gmail.com>
2023-10-17 15:19:39 -0500

    update app.js

--- app.js
+++ app.js
@@ -3,12 +3,18 @@ const entrada = document.querySelector('.filter');
 let caja = document.querySelector('.grafic');
 
 function MostrarData(data, input) {
-    let distritos = data.filter(obj => obj && obj.NOMBDEP == input);
-    let count = distritos.length;
-    console.log(`Departamento: ${input}, cantidad de Distritos: ${count}`);
+    //CANTIDAD DE OBJECTOS CON EL MISMO DEPARTAMENTO
+    let obj_departamento = data.filter(obj => obj && obj.NOMBDEP == input);
+    
+    //NOMBRES DE PROVINCIAS POR DEPARTAMENTO
+    let provincias = obj_departamento.map(obj => obj.NOMBPROV);
+    let names_provincias = new Set(provincias);
+
+    //CANTIDAD DE DISTRITOS POR DEPARTAMENTO
+    let countDistritos = obj_departamento.length;
+
+    console.log(`Departamento: ${input} Provincias: ${Array.from(names_provincias)} Distritos: ${countDistritos}`);
 };
-
-
 
 btn.addEventListener('click', () => {
     const input = entrada.value.toUpperCase();
@@ -28,4 +34,4 @@ btn.addEventListener('click', () => {
 //     "NOM_CAPITAL (LEGAL)": "CHACHAPOYAS",
 //     "COD_ REG_NAT": 2,
 //     "REGION NATURAL": "SIERRA"
-//    },
+//    }

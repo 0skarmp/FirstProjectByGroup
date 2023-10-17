2502bed7c40413ae0c2ff3c19527795cae703f28
JuanRCoder <guillecarck@gmail.com>
2023-10-17 12:42:54 -0500

    update project

---
+++ app.js
@@ -0,0 +1,19 @@
+const btn = document.querySelector('.button');
+const entrada = document.querySelector('.filter');
+const caja = document.querySelector('.grafic');
+
+function MostrarData(data,departament) {
+    // let departaments = data.filter(dep => dep.NOMBDEP === departament);
+    // caja.innerHTML = "";
+    console.log(data, departament);  
+}
+
+btn.addEventListener('click', () => {
+    const val = entrada.value;
+    
+    // Realizar la solicitud usando fetch
+    fetch("https://raw.githubusercontent.com/0skarmp/FirstProjectByGroup/main/UBIGEO.json")
+    .then(res => res.json())
+    .then(data => MostrarData(data, val))
+    .catch(error => console.log(error));
+});
